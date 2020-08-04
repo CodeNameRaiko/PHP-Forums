362c12850305bb600bfd8fd18e2701c2596ea952
[CD] Raiko <64981218+CodeNameRaiko@users.noreply.github.com>
2020-08-04 03:25:03 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,22 @@
+<footer>
+        <ul>
+          <li><b>Docs</b></li>
+            <li><a href="home.php">Home</a></li>
+            <li><a href="forum.php">Forum</a></li>
+            <li><a href="profile.php">Profile</a></li>
+        </ul>
+     
+        <ul>
+          <li><b>Social Media</b></li>
+          <li><a href="#">Facebook</a></li>
+          <li><a href="#">Instagram</a></li>
+          <li><a href="#">Discord</a></li>
+        </ul>
+     
+        <ul>
+          <li><b>Developers</b></li>
+          <li><a href="#">Github</a></li>
+          <li><a href="#">Codepen</a></li>
+          <li><a href="#">Dev Com.</a></li>
+        </ul>
+    </footer>
