1a881855613bd7ba40df7f40457f1f4ff5fedcab
[CD] Raiko <64981218+CodeNameRaiko@users.noreply.github.com>
2020-08-04 03:22:04 +0200

    Create start.php
    Starting page

---
+++ start.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <!-- Stylesheets -->
+        <link rel="stylesheet" href="./stylesheets/style.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+
+        <title>Forums</title>
+    </head>
+<body>
+    <nav>
+            <ul class="ul">
+                <span>
+                    <li class="logo li"><img src="logo.png" class="logo-a img"></li>
+                </span>
+                <li class="li"><a href="forum.php">Forums</a></li>
+                    <li class="li"><a href="profile.php">Profile</a></li>
+                </ul>
+    </nav>
+    <div class="wrapper">
+        <h1 class="lead">Welcome to the forum!</h1>
+        <div class="b-align">
+            <a href="login.php" class="b b1">Login</a>
+        
+            <a href="signUp.php" class="b b2">Sign Up</a>
+        </div>
+    </div>
+<?php include('./templates/footer.php');?>
+</body>
+</html>
+
+
+
+
+
+
+
+
